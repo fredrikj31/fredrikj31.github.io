8d1936ca3cecfcdeeb5a549d80194f9b8c362802
Fredrik Johansen <fredrik1206@gmail.com>
2022-06-01 13:13:47 +0200

    Added a limit and dynamic projects

--- assets/js/projects-displayer.js
+++ assets/js/projects-displayer.js
@@ -1,8 +1,12 @@
 class ProjectDisplayer {
+	projectLimit = null;
+
 	/**
 	 * @description This function runs the rendering proces of the projects
 	 */
-	constructor() {
+	constructor(limit) {
+		this.projectLimit = limit;
+
 		// Calling the initialize function
 		this.displayProjects();
 	}
@@ -52,38 +56,38 @@ class ProjectDisplayer {
 
 		const projectsElem = document.getElementById("projects");
 
-		data.forEach(element => {
+		for (let index = 0; index < (this.projectLimit === null ? data.length + 1 : this.projectLimit); index++) {
+			const element = data[index];
+			
 			const tempElem = document.createElement("div");
-			tempElem.classList.add("project");
+			tempElem.classList.add("isotope-item", "col-md-6", "mb-5");
 
 			tempElem.innerHTML = `
-				<div class="isotope-item col-md-6 mb-5">
-					<div class="card project-card">
-						<div class="row no-gutters">
-							<div class="col-lg-4 card-img-holder">
-								<img src="${element["image"]}" class="card-img" alt="Picture of the project ${element["title"]}">
-							</div>
-							<div class="col-lg-8">
-								<div class="card-body">
-									<h5 class="card-title"><a href="${element["link"]}" class="theme-link">${element["title"]}</a></h5>
-									<p class="card-text">${element["description"]}</p>
-									<p class="card-text"><small class="text-muted">${element["footer"]}</small></p>
-								</div>
+				<div class="card project-card">
+					<div class="row no-gutters">
+						<div class="col-lg-4 card-img-holder">
+							<img src="${element["image"]}" class="card-img" alt="Picture of the project ${element["title"]}">
+						</div>
+						<div class="col-lg-8">
+							<div class="card-body">
+								<h5 class="card-title"><a href="${element["link"]}" class="theme-link">${element["title"]}</a></h5>
+								<p class="card-text">${element["description"]}</p>
+								<p class="card-text"><small class="text-muted">${element["footer"]}</small></p>
 							</div>
 						</div>
-						<div class="link-mask">
-							<a class="link-mask-link" href="${element["link"]}"></a>
-							<div class="link-mask-text">
-								<a class="btn btn-secondary" href="${element["link"]}">
-									<i class="fas fa-eye mr-2"></i>View Case Study
-								</a>
-							</div>
+					</div>
+					<div class="link-mask">
+						<a class="link-mask-link" href="${element["link"]}"></a>
+						<div class="link-mask-text">
+							<a class="btn btn-secondary" href="${element["link"]}">
+								<i class="fas fa-eye mr-2"></i>View Case Study
+							</a>
 						</div>
 					</div>
 				</div>
 			`;
 
 			projectsElem.appendChild(tempElem);
-		});
+		}
 	}
 }
