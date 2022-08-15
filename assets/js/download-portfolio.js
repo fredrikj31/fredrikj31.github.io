82cc55aa4c67a28c94e6dfcf62ae7517676d96a4
Fredrik Johansen <fredrik1206@gmail.com>
2022-08-15 16:30:28 +0200

    Fixed some events

--- assets/js/download-portfolio.js
+++ assets/js/download-portfolio.js
@@ -1,4 +1,4 @@
 function downloadPortfolio(type) {
-	if (type === "English") umami.trackEvent('Portfolio Download', { type: "English" }, '/portfolio', '963c80f6-c70d-4d10-8f99-6dde4010f4f9');
-	if (type === "Danish") umami.trackEvent('Portfolio Download', { type: "Danish" }, '/portfolio', '963c80f6-c70d-4d10-8f99-6dde4010f4f9');
+	if (type === "English") umami.trackEvent('resume-download', { type: "English" }, '/portfolio', '963c80f6-c70d-4d10-8f99-6dde4010f4f9');
+	if (type === "Danish") umami.trackEvent('resume-download', { type: "Danish" }, '/portfolio', '963c80f6-c70d-4d10-8f99-6dde4010f4f9');
 }
